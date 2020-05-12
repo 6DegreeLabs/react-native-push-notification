67e714eea4bb02729c38b70fa23c5d507d64acef
Brian DeWeese <brian@briandeweese.org>
2020-05-12 16:12:09 -0700

    use correct component

--- component/index.ios.js
+++ component/index.ios.js
@@ -1,9 +1,12 @@
 'use strict';
 
 import {
-  AppState,
-  PushNotificationIOS
+  AppState
 } from 'react-native';
+
+import {
+  PushNotificationIOS
+} from '@react-native-community/push-notification-ios'
 
 module.exports = {
   state: AppState,
